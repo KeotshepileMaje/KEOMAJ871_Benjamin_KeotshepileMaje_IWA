9a0ce605167c7ed09cf68a8292e6c90b2d362981
keotshepileMaje <keotshepilemaje@gmail.com>
2023-03-27 20:27:24 +0200

    JS Added

--- scripts.js
+++ scripts.js
@@ -1,5 +1,29 @@
-const number = document.querySelector('[data-key="number"')
-const subtract = document.querySelector('[data-key="substract"')
-const add = document.querySelector('[data-key="add"')
+const MAX_NUMBER = 15;
+const MIN_NUMBER = -5;
+const STEP_AMOUNT = 5;
 
-console.log(number, subtract, add)
+const number = document.querySelector('[data-key="number"]')
+const subtract = document.querySelector('[data-key="substract"]')
+const add = document.querySelector('[data-key="add"]')
+
+const subtractHandler = () => {
+    const newValue = parseInt(number.value) - STEP_AMOUNT
+    number.value = newValue 
+
+    if (add.disabled === true) {
+        add.disabled = false
+    }
+    if (newValue <= MIN_NUMBER) {
+        subtract.disabled = true  
+    }
+}
+const addHandler = () =>{
+    const newValue = parseInt(number.value) + STEP_AMOUNT
+    number.value = newValue 
+
+    if (subtract.disabled === true) {
+        subtract.disabled = false
+    } 
+}
+subtract.addEventListener('click', subtractHandler);
+add.addEventListener('click', addHandler);
