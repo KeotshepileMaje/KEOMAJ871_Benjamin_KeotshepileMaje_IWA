15b50771f9d9512f11c6907d68c7a92491e973ec
keotshepileMaje <keotshepilemaje@gmail.com>
2023-04-04 11:56:14 +0200

    Removed the comment

--- IWA3/Challenge-1/scripts-3.1.js
+++ IWA3/Challenge-1/scripts-3.1.js
@@ -1,14 +1,6 @@
 import {company, year} from "./configuration.js";
-// import {year} from "./configuration.js";
-
-
 
 console.log(company, year);
 
-const message = '© ' + company + '(' + year + ')'
+const message = '© ' + company + ' ('+ year + ')'
 document.querySelector('footer').innerText = message
-
-
-
-// const message = "© " + company + ' '+ year
-// document.querySelector('footer').innerText = message
