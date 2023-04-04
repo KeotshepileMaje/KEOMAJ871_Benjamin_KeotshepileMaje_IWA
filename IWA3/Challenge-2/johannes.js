1dffdfebe8069667672f7caef229c04fdb2d01a8
keotshepileMaje <keotshepilemaje@gmail.com>
2023-04-04 11:56:35 +0200

    Simplified the code

--- IWA3/Challenge-2/johannes.js
+++ IWA3/Challenge-2/johannes.js
@@ -1,3 +1,6 @@
- export const firstname2 = "Johannes"
- export const surname2 = "Potgieter"
- export const role2 = "Intern"
+const firstname2 = "Johannes"
+const surname2 = "Potgieter"
+export const role2 = "Intern"
+
+const display2 = firstname2 + " " + surname2 + " (" + role2 + ")"
+document.querySelector('#johannes').innerText = display2
