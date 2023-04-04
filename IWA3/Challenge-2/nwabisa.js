7be2293fd95d1bc0c150d6f4beb9e6422c8cc7f2
keotshepileMaje <keotshepilemaje@gmail.com>
2023-04-04 11:56:40 +0200

    Simplified the code

--- IWA3/Challenge-2/nwabisa.js
+++ IWA3/Challenge-2/nwabisa.js
@@ -1,4 +1,7 @@
-export const  firstname = "Nwabisa"
-export const surname = "Gabe"
+const  firstname = "Nwabisa"
+const surname = "Gabe"
 export const role = "CEO"
+
+const display= firstname + " " + surname + "(" + role + ")"
+document.querySelector('#nwabisa').innerText = display
 
