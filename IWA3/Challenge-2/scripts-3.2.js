1d3f759859b94d3e28c5d8956bf09c7bc551a3cf
keotshepileMaje <keotshepilemaje@gmail.com>
2023-04-04 11:57:30 +0200

    Simplified the code

--- IWA3/Challenge-2/scripts-3.2.js
+++ IWA3/Challenge-2/scripts-3.2.js
@@ -1,14 +1,14 @@
-import {firstname3, surname3, role3} from './alex.js'
-import {firstname2, surname2, role2} from './johannes.js'
-import {firstname, surname, role} from './nwabisa.js'
+import {role3} from './alex.js'
+import {role2} from './johannes.js'
+import {role} from './nwabisa.js'
 
-const display3 = firstname3 + " " + surname3 + " (" + role3 + ")"
-document.querySelector('#alex').innerText = display3
+// const display3 = firstname3 + " " + surname3 + " (" + role3 + ")"
+// document.querySelector('#alex').innerText = display3
 
-const display2 = firstname2 + " " + surname2 + " (" + role2 + ")"
-document.querySelector('#johannes').innerText = display2
+// const display2 = firstname2 + " " + surname2 + " (" + role2 + ")"
+// document.querySelector('#johannes').innerText = display2
 
-const display= firstname + " " + surname + " (" + role + ")"
-document.querySelector('#nwabisa').innerText = display
+// const display= firstname + " " + surname + "(" + role + ")"
+// document.querySelector('#nwabisa').innerText = display
 
 console.log("Roles:", role, ",",role2,",", role3)
