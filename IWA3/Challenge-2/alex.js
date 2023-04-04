d50a34ea43881d2fd7eea0c7d290b1e06a6cb1fc
keotshepileMaje <keotshepilemaje@gmail.com>
2023-04-04 11:56:29 +0200

    Simplified the code

--- IWA3/Challenge-2/alex.js
+++ IWA3/Challenge-2/alex.js
@@ -1,4 +1,7 @@
-export const firstname3 = "Alex"
-export const surname3 = "Naidoo"
+const firstname3 = "Alex"
+const surname3 = "Naidoo"
 export const role3 = "Head of Marketing"
 
+
+const display3 = firstname3 + " " + surname3 + " (" + role3 + ")"
+document.querySelector('#alex').innerText = display3
