4ca3d52655487754cc511a478bafaddfe782af1c
keotshepileMaje <keotshepilemaje@gmail.com>
2023-04-05 16:27:00 +0200

    Code Simplified

--- IWA5/IWA-5.js
+++ IWA5/IWA-5.js
@@ -4,8 +4,8 @@ const NONE_SELECTED = 0
 
 //Gloabal variables
 let getlocation = "RSA";
-let currency;
-let shipping; 
+let currency = null;
+let shipping = null; 
 const customers = 1
 
 //Buying conditions
@@ -13,14 +13,14 @@ if (getlocation === 'RSA') {
 	shipping = 400
 	currency = 'R'
 } else {
-	shipping = 800
+	shipping = getlocation === 'NAM' ? 600 : 800
 	currency = '$'	
 }
 
-if (getlocation === 'NAM'){
-    shipping = 600
-	currency = "$"
-}
+// if (getlocation === 'NAM'){
+//     shipping = 600
+// 	currency = "$"
+// }
 
 //Items to buy
 const shoes = 300 * 1
@@ -28,13 +28,14 @@ const toys = 100 * 5
 const shirts = 150 * NONE_SELECTED
 const batteries = 35 * 2
 const pens = 5 * NONE_SELECTED 
+const total = shoes + toys + batteries + pens + shirts
 
 //Delivery conditions
-if ( ( (shoes + toys + batteries + pens + shirts) >= 1000 && 
+if ( ( (total) >= 1000 && 
 	(currency === 'R') && (customers === 1) && 
 	(getlocation === ('NAM' || 'RSA')) ) 
         ||
-	( (shoes + toys + batteries + pens + shirts) >= 60 && (currency === '$') &&(customers === 1) && 
+	( (total) >= 60 && (currency === '$') &&(customers === 1) && 
 	(getlocation === ('NAM' || 'RSA')) ) ){
 	shipping = 0
 	} 
--- IWA6/challenge-1/IWA-6.1.js
+++ IWA6/challenge-1/IWA-6.1.js
@@ -1,16 +1,27 @@
+/*
+ *This is a more recognised method
+ */
 const primaryPhone = 'O748105141'
 const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-const  onlyNumbers = /^[0-9]+$/ 
-
-const primaryValid = onlyNumbers.test(primaryPhone) 
-// .test() -->Tests if this string contain only digits.
-const secondaryValid = onlyNumbers.test(secondaryPhone)
+const primaryValid = !Number.isNaN(parseInt(primaryPhone))
+const secondaryValid = !Number.isNaN(parseInt(secondaryPhone))
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
+
+/*
+ *Another method
+ */
+// const primaryPhone = 'O748105141'
+// const secondaryPhone = '0219131568'
+// const  onlyNumbers = /^[0-9]+$/ 
+// const primaryValid = onlyNumbers.test(primaryPhone) 
+// const secondaryValid = onlyNumbers.test(secondaryPhone)
+// console.log('Primary phone is valid numerical string:', primaryValid)
+// console.log('Secondary phone is valid numerical string:', secondaryValid )
 
 // /^[0-9]+$/ -->This regular expression matches any string that consists of only digits from 0-9 and nothing else.
 // .test() -->Tests if this string contain only digits.
