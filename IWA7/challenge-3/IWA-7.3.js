876260e0a6765fa137f67d039e9d48d085dc550b
keotshepileMaje <keotshepilemaje@gmail.com>
2023-04-06 16:20:04 +0200

    Simplified the code

--- IWA7/challenge-3/IWA-7.3.js
+++ IWA7/challenge-3/IWA-7.3.js
@@ -1,21 +1,21 @@
 const leoName = 'Leo'
 const leoSurname = 'Musvaire'
-const leoBalance = '9394'
+const leoBalance = '-9394'
 
 const sarahName = 'Sarah'
 const sarahSurname = 'Kleinhans'
-const sarahBalance = '4582.21000111'
+const sarahBalance = '-4582.21000111'
 
 const divider = '----------------------------------'
 
 // Only change below this line
 
 
-const owed = ( parseFloat((1*leoBalance) + (1*sarahBalance)) ).toFixed(2)
+const owed = ((-1*leoBalance) + (-1*sarahBalance))
 
-const leo = `${leoName}  ${leoSurname}  (Owed: R ${leoBalance})`
-const sarah = `${sarahName}  ${sarahSurname}  (Owed:  R ${sarahBalance})`
-const total = `Total amount owed: R ${owed}`
+const leo = `${leoName}  ${leoSurname}  (Owed: R ${-leoBalance})`
+const sarah = `${sarahName}  ${sarahSurname}  (Owed:  R ${parseFloat(-sarahBalance).toFixed(2)})`
+const total = `Total amount owed: R ${owed.toFixed(2)}`
 const result = `\n${leo} \n${sarah} \n${divider} \n${total} \n${divider} `
 console.log(result)
 
