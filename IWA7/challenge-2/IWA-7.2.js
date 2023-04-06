ddb23fb318f05938b1f782329dc0911a09776fba
keotshepileMaje <keotshepilemaje@gmail.com>
2023-04-06 16:19:40 +0200

    Simplified the Code

--- IWA7/challenge-2/IWA-7.2.js
+++ IWA7/challenge-2/IWA-7.2.js
@@ -1,10 +1,10 @@
 const nickname= "Timmy";
 const firstname = "Timothy";
-const user = `${(nickname || firstname)}`
+const user = (nickname || firstname)
 const ans1 = `Good Morning, ${user}!`
 const ans2 = `Good Morning!`
 
-if (user || nickname || firstname){
+if (user){
     console.log(ans1)
 }else {
     console.log(ans2)
