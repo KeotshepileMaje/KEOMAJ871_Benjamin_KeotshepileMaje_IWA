e0ea2210fbf376c7a648a7b2bf09942bb45f5cb6
keotshepileMaje <keotshepilemaje@gmail.com>
2023-04-14 16:45:33 +0200

    Added method 3 as commented

--- IWA11/IWA-11.js
+++ IWA11/IWA-11.js
@@ -34,7 +34,6 @@ Status_3.textContent = Root_3.getAttribute('data-delivered') === 'true' ? 'Deliv
 
 /*--------------------------*/
 
-
 /*
  *Method 2
  *Solution from Chatgpt
@@ -71,3 +70,54 @@ Status_3.textContent = Root_3.getAttribute('data-delivered') === 'true' ? 'Deliv
 // })
 
 
+/*-----------------------------*/
+/*
+*Method 3
+*for my learner buddy*/
+
+// // Order1
+// const order1Root = document.querySelector('body > section:nth-child(3) > dl');
+
+// const order1Biscuits = document.querySelector('body section:nth-child(3) dl  div.biscuits dd');
+// order1Biscuits.innerText = order1Root.dataset.biscuits ;
+
+// const order1Donuts = document.querySelector('body > section:nth-child(3) > dl > div.donuts > dd')
+// order1Donuts.innerText = order1Root.dataset.donuts;
+
+// const order1Pancakes = document.querySelector('body > section:nth-child(3) > dl > div.pancakes > dd')
+// order1Pancakes.innerText = order1Root.dataset.pancakes;
+
+// const order1Status = document.querySelector('body > section:nth-child(3) > dl > div.status > dd')
+// order1Status.innerText = order1Root.dataset.delivered === 'true' ? 'Delivered' : 'Pending';
+
+// /*Called the root by adding order1 and ensuring that I camelCase. I used inspect to copy selectior of the path i wanted in the elements tab.
+// Ussed innerText to get the iner text of the particular element and dataSet to get values of data attributes that are on the HTML.*/
+// //Order 2
+// const order2Root = document.querySelector('body > section:nth-child(4) > dl');
+
+// const order2Biscuits = document.querySelector('body > section:nth-child(4) > dl > div.biscuits > dd');
+// order2Biscuits.innerText = order2Root.dataset.biscuits ;
+
+// const order2Donuts = document.querySelector('body > section:nth-child(4) > dl > div.donuts > dd')
+// order2Donuts.innerText = order2Root.dataset.donuts;
+
+// const order2Pancakes = document.querySelector('body > section:nth-child(4) > dl > div.pancakes > dd')
+// order2Pancakes.innerText = order2Root.dataset.pancakes;
+
+// const order2Status = document.querySelector('body > section:nth-child(4) > dl > div.status > dd')
+// order2Status.innerText = order2Root.dataset.delivered === 'true' ? 'Delivered' : 'Pending';
+
+// // Order 3
+// const order3Root = document.querySelector('body > section:nth-child(5) > dl');
+
+// const order3Biscuits = document.querySelector('body > section:nth-child(5) > dl > div.biscuits > dd');
+// order3Biscuits.innerText = order3Root.dataset.biscuits ;
+
+// const order3Donuts = document.querySelector('body > section:nth-child(5) > dl > div.donuts > dd')
+// order3Donuts.innerText = order3Root.dataset.donuts;
+
+// const order3Pancakes = document.querySelector('body > section:nth-child(5) > dl > div.pancakes > dd')
+// order3Pancakes.innerText = order3Root.dataset.pancakes;
+
+// const order3Status = document.querySelector('body > section:nth-child(5) > dl > div.status > dd')
+// order3Status.innerText = order3Root.dataset.delivered === 'true' ? 'Delivered' : 'Pending';
