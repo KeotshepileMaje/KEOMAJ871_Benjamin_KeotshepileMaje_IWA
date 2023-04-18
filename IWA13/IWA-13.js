789de077544189fda777ea1a7ea46cb8416298e9
keotshepileMaje <keotshepilemaje@gmail.com>
2023-04-18 23:56:05 +0200

    Debugged

--- IWA13/IWA-13.js
+++ IWA13/IWA-13.js
@@ -8,8 +8,7 @@ let calculated = '1'
 const logCalc = () => { 
     const isString = typeof calculated == 'string' 
     let calculatedAsNumber = isString ? parseInt(calculated) : calculated
-    console.log(calculatedAsNumber)
-    calculated = calculatedAsNumber + 6 ;
+    calculated = calculatedAsNumber + 2 ;
 }
 
 const calcUser = () => {  
@@ -17,35 +16,34 @@ const calcUser = () => {
   if (calculated >= 2) {
       user = 'John'
       state = 'requesting'
-      console.log('great')
+      //console.log('great')
       } 
   if (calculated > 3) { 
       state = 'idle'
-      console.log('oih')
+      //console.log('oih')
 }
 }
-calcUser()
 
 const checkUser = () => {
   calcUser()
-	if (user && state === 'requesting') {
+	if ( user && state === 'requesting') {
 		console.log(`User: ${user} (${calculated})`)
 	}
 }
-
-// Only allowed to change code above
-
 // checkUser()
-// calcUser()
+// //Only allowed to change code above
 
-// checkUser()
-// calcUser()
+checkUser()
+calcUser()
 
-// checkUser()
-// calcUser()
+checkUser()
+calcUser()
 
-// checkUser()
-// calcUser()
+checkUser()
+calcUser()
 
-// checkUser()
-// calcUser()
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
