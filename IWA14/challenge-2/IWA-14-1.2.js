20b476f3c939659ab5e208b46069e1b1fdaea9c2
keotshepileMaje <keotshepilemaje@gmail.com>
2023-04-19 13:58:25 +0200

    Removed commented code

--- IWA14/challenge-2/IWA-14-1.2.js
+++ IWA14/challenge-2/IWA-14-1.2.js
@@ -1,38 +1,8 @@
-// const counter = {
-//     value: 120, 
-//     add: function() {
-//         this.value = this.value + 1 
-//     },
-//     minus: function() {
-//         this.value = this.value - 1
-//     },
-// }
-
-// counter.add()
-// counter.add()
-// counter.add()
-// counter.minus()
-// console.log(counter.value)
-
-
-// //function that adds two numbers 
-// const sum = (a, b) => {
-//     return a + b;
-//   }
-  
-//   // calling sum() function  
-//   var result = sum.call(sum, 5 , 10);
-  
-//   console.log(result);
-  
-//   //Output:
-//   // 15
-
 // script.js
 
 const add = (a, b) => a + b 
 
-const multiply = ( a, b) =>  a * b 
+const multiply = ( a, b) =>  a * b
 
 function internal() {
 	const added = this.add(this.internal.a, this.internal.b)
